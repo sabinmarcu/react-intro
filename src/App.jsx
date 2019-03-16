ba0b9974f268bbd88822c516af170ea62c392133
Sabin Marcu <sabin.marcu@rga.com>
2019-03-16 04:53:21 +0200

    feat: implement base setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prefer-stateless-function */
+
+import React, { Component } from 'react';
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
+import theme from './assets/react-toolbox/theme';
+import Home from './routes/Home';
+
+import './assets/react-toolbox/theme.css';
+
+class App extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <Home />
+      </ThemeProvider>
+    );
+  }
+}
+
+export default App;

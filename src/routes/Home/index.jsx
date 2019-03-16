41b890ee2cfd10125b3d7acebc2435e59a3e9843
Sabin Marcu <sabin.marcu@rga.com>
2019-03-16 06:01:25 +0200

    added server

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppBar from 'react-toolbox/lib/app_bar/AppBar';
 import Navigation from 'react-toolbox/lib/navigation/Navigation';
 import Link from 'react-toolbox/lib/link/Link';
 import Movie from '../../components/MovieComponent';
 
 import styles from './style.module.css';
-import data from './data.json';
 
-export default () => (
-  <div className={styles.wrapper}>
-    <AppBar title="Movie Database">
-      <Navigation type="horizontal">
-        <Link href="http://jsleague.ro" active>JSLeague</Link>
-        <Link href="https://reactjs.org/" active>React</Link>
-        <Link href="http://react-toolbox.io/#/">React Toolbox</Link>
-      </Navigation>
-    </AppBar>
-    <div className={styles.list}>
-      {data.movies.map(({ id, ...rest }) => (
-        <Movie key={id} {...rest} />
-      ))}
-    </div>
-  </div>
-);
+export default class HomeComponent extends Component {
+  state = {
+    movies: [],
+  }
+
+  componentWillMount() {
+    fetch('http://localhost:8000/movies')
+      .then(data => data.json())
+      .then(movies => this.setState({ movies }));
+  }
+
+  render() {
+    const { movies } = this.state;
+    return (
+      <div className={styles.wrapper}>
+        <AppBar title="Movie Database">
+          <Navigation type="horizontal">
+            <Link href="http://jsleague.ro" active>JSLeague</Link>
+            <Link href="https://reactjs.org/" active>React</Link>
+            <Link href="http://react-toolbox.io/#/">React Toolbox</Link>
+          </Navigation>
+        </AppBar>
+        <div className={styles.list}>
+          {movies.map(({ id, ...rest }) => (
+            <Movie key={id} {...rest} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
